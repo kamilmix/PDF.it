eec5d22889e90174c44bede8f50632240dfb650b
Kamil Mucha <kamilmix@gmail.com>
2020-12-02 22:15:45 +0100

    Add split PDF functions

---
+++ PDF.it/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using iText.Kernel.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF.it
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var filePath = GetFilePath();
+            SplitPdf(filePath);     
+        }
+
+        private string GetFilePath()
+        {
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            return filePath;
+        }
+
+        private void SplitPdf(string filePath)
+        {
+            using (var pdfDoc = new PdfDocument(new PdfReader(filePath)))
+            {
+                var outputDir = @"E:\test\";
+                var splitter = new CustomSplitter(pdfDoc, outputDir);
+                var splittedDocs = splitter.SplitByPageCount(1);
+
+                foreach (var splittedDoc in splittedDocs)
+                {
+                    splittedDoc.Close();
+                }
+            }
+        }
+    }
+}
---
+++ PDF.it/Utils/CustomSplitter.cs
@@ -0,0 +1,27 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF.it
+{
+    class CustomSplitter : PdfSplitter
+    {
+        private int _order;
+        private readonly string _destinationFolder;
+
+        public CustomSplitter(PdfDocument pdfDocument, string destinationFolder) : base(pdfDocument)
+        {
+            _destinationFolder = destinationFolder;
+            _order = 0;
+        }
+
+        protected override PdfWriter GetNextPdfWriter(PageRange documentPageRange)
+        {
+            return new PdfWriter(_destinationFolder + "splitDocument1_" + _order++ + ".pdf");
+        }
+    }
+}
